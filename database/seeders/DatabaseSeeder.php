4e14ccac3d818db07c45fff2fb55fa458533125a
Francesco1276 <francescodeachjullyocessare@smkwikrama.sch.id>
2023-05-15 12:47:13 +0700

    Interview

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345'),
+            'role' => 'admin',
+           ]);
+
+           User::create([
+            'name' => 'petugas',
+            'email' => 'petugas@gmail.com',
+            'password' => bcrypt('12345'),
+            'role' => 'petugas',
+           ]);
+    }
+}
